e1d060c753c16065862a830ca0d093d1b993e72e
Graham Steel <graham@steelworks.org.uk>
2021-02-08 11:57:24 +0000

    SermonPodcastPages: refactoring, restyling

--- SermonPodcastPages/MainForm.cs
+++ SermonPodcastPages/MainForm.cs
@@ -1,13 +1,7 @@
 ﻿using Excel;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SermonPodcastPages
@@ -57,32 +51,23 @@ namespace SermonPodcastPages
                     htmlWriter.WriteLine( "<HTML>" );
 
                     htmlWriter.WriteLine( "<HEAD>" );
-                    htmlWriter.WriteLine( "<TITLE>Bethel Podcasts {0}</TITLE>", sheet.TableName );
+                    htmlWriter.WriteLine( $"<TITLE>Bethel Podcasts {sheet.TableName}</TITLE>");
                     htmlWriter.WriteLine(bootstrapIncludes);
-
-                    //AddOriginalStyle(htmlWriter);
-                    //AddColourfulStyle(htmlWriter);
 
                     htmlWriter.WriteLine( "</HEAD>" );
 
                     htmlWriter.WriteLine("<BODY>");
                     string year = sheet.TableName;
                     WriteYearIndex(htmlWriter, year, result.Tables);
-                    htmlWriter.WriteLine(@"<div class=""container"">");
+                    htmlWriter.WriteLine("<div class=\"container\">");
                     htmlWriter.WriteLine("<BR><BR>");
-                    htmlWriter.WriteLine( "<H1>Bethel Macclesfield sermons {0}</H1>", year );
+                    htmlWriter.WriteLine( $"<H1>Bethel Macclesfield sermons {year}</H1>" );
 
                     htmlWriter.WriteLine( "<BR>" );
-                    //htmlWriter.WriteLine( @"<TABLE style=""width:800px"">" );
 
                     if (sheet.TableName == DateTime.Now.Year.ToString())
                     {
-                        htmlWriter.WriteLine(@"<div class=""well"">");
-                        htmlWriter.WriteLine("<H3>For the latest sermons, please visit our Podbean site</H3>");
-                        htmlWriter.WriteLine(@"    <a href=""https://bethelmacc.podbean.com/"" target=""_blank"" class=""btn btn-info"">");
-                        htmlWriter.WriteLine(@"      <span class=""glyphicon glyphicon-share-alt""></span> Bethel Macclesfield on Podbean");
-                        htmlWriter.WriteLine(@"    </a>");
-                        htmlWriter.WriteLine("</div>");
+                        RedirectToPodbean(htmlWriter);
                     }
                     else
                     {
@@ -92,9 +77,8 @@ namespace SermonPodcastPages
                         }
                     }
 
-                    //htmlWriter.WriteLine( "</TABLE>" );
-                    htmlWriter.WriteLine( @"<A id=""latest""> </A>" );
-                    htmlWriter.WriteLine(@"</div>");
+                    htmlWriter.WriteLine( "<A id=\"latest\"> </A>" );
+                    htmlWriter.WriteLine("</div>");
                     htmlWriter.WriteLine( "</BODY>" );
                     htmlWriter.WriteLine( "</HTML>" );
                 }
@@ -104,69 +88,34 @@ namespace SermonPodcastPages
         }
 
         /// <summary>
-        /// CSS styling for a simple HTML table
+        /// Avoid excessive traffic to the web host: for the current year, redirect the user to Podbean
         /// </summary>
-        static void AddOriginalStyle(TextWriter aWriter)
+        /// <param name="htmlWriter"></param>
+        static void RedirectToPodbean(TextWriter htmlWriter)
         {
-            aWriter.WriteLine("<STYLE>");
-            aWriter.WriteLine("P.SERIF{font-family:\"Times New Roman\",Times,serif;}");
-            aWriter.WriteLine("P.SANSSERIF{font-family:Tahoma,Helvetica,sans-serif;}");
-            aWriter.WriteLine("TABLE,TH,TD");
-            aWriter.WriteLine("{");
-            aWriter.WriteLine("    border:1px solid black;");
-            aWriter.WriteLine("}");
-            aWriter.WriteLine("TD");
-            aWriter.WriteLine("{");
-            aWriter.WriteLine("padding:15px;");
-            aWriter.WriteLine("}");
-            aWriter.WriteLine("</STYLE>");
-        }
-
-        /// <summary>
-        /// CSS styling for a colourful HTML table
-        /// </summary>
-        static void AddColourfulStyle(TextWriter aWriter)
-        {
-            aWriter.WriteLine("<STYLE>");
-            //aWriter.WriteLine("P.SERIF{font-family:\"Times New Roman\",Times,serif;}");
-            //aWriter.WriteLine("P.SANSSERIF{font-family:Tahoma,Helvetica,sans-serif;}");
-
-            aWriter.WriteLine("table {");
-            aWriter.WriteLine("       border-collapse: collapse;");
-            aWriter.WriteLine("       width: 100 %;");
-            aWriter.WriteLine("}");
-
-            aWriter.WriteLine("th, td {");
-            aWriter.WriteLine("       text-align: left;");
-            aWriter.WriteLine("       padding: 8px;");
-            aWriter.WriteLine("       border-bottom: 1px solid #ddd;");
-            aWriter.WriteLine("}");
-
-            aWriter.WriteLine("tr:nth-child(even){background-color: #f2f2f2}");
-            aWriter.WriteLine("tr:hover {background-color: #fff5f5}");
-
-            aWriter.WriteLine("th {");
-            aWriter.WriteLine("       background-color: #b2f2b2;");
-            aWriter.WriteLine("       color: green;");
-            aWriter.WriteLine("}");
-            aWriter.WriteLine("</STYLE>");
+            htmlWriter.WriteLine("<div class=\"well\">");
+            htmlWriter.WriteLine("<H3>For the latest sermons, please visit our Podbean site</H3>");
+            htmlWriter.WriteLine("    <a href=\"https://bethelmacc.podbean.com/\" target=\"_blank\" class=\"btn btn-info\">");
+            htmlWriter.WriteLine("      <span class=\"glyphicon glyphicon-share-alt\"></span> Bethel Macclesfield on Podbean");
+            htmlWriter.WriteLine("    </a>");
+            htmlWriter.WriteLine("</div>");
         }
 
         /// <summary>
         /// If we are writing this year's sheet, then show a link to the latest sermon at the bottom
         /// of the page
         /// </summary>
-        static void WriteLinkToLatest( TextWriter aWriter, DataTable aSheet )
+        static void WriteLinkToLatest( TextWriter htmlWriter, DataTable sheet )
         {
-            if ( aSheet.TableName == DateTime.Now.Year.ToString() )
+            if ( sheet.TableName == DateTime.Now.Year.ToString() )
             {
-                aWriter.WriteLine( @"<BR>" );
-                aWriter.WriteLine( @"<DIV style=""background-color:#FFA500;clear:both;text-align:center;width:800px;"">" +
-                                      @"<A href=""#latest"">Go to latest sermon</A></DIV>" );
+                htmlWriter.WriteLine( "<BR>" );
+                htmlWriter.WriteLine( "<DIV style=\"background-color:#FFA500;clear:both;text-align:center;width:800px;\">" +
+                                      "<A href=\"#latest\">Go to latest sermon</A></DIV>" );
             }
         }
 
-        void WriteYearIndex( TextWriter aWriter, string aThisYear, DataTableCollection aYears  )
+        void WriteYearIndex( TextWriter htmlWriter, string thisYear, DataTableCollection years  )
         {
             const string navbarBegin = @"
 <nav class=""navbar navbar-inverse navbar-fixed-top"">
@@ -177,7 +126,7 @@ namespace SermonPodcastPages
         <span class=""icon-bar""></span>
         <span class=""icon-bar""></span>
       </button>
-      <a class=""navbar-brand"" href=""http://www.bethelmacclesfield.org.uk"">Bethel Macclesfield</a>
+      <a class=""navbar-brand"" href=""https://www.bethelmacclesfield.org.uk"">Bethel Macclesfield</a>
     </div>
     <div class=""collapse navbar-collapse"" id=""myNavbar"">
       <ul class=""nav navbar-nav"">
@@ -193,158 +142,101 @@ namespace SermonPodcastPages
 </nav>
                 ";
 
-            aWriter.WriteLine(navbarBegin);
+            htmlWriter.WriteLine(navbarBegin);
 
-            foreach ( DataTable year in aYears )
+            foreach ( DataTable year in years )
             {
-                if ( year.TableName == aThisYear )
+                if ( year.TableName == thisYear )
                 {
-                    aWriter.WriteLine(@"<li class=""active""><a href=""{0}.html"">{0}</a></li>", year.TableName);
+                    htmlWriter.WriteLine($"<li class=\"active\"><a href=\"{year.TableName}.html\">{year.TableName}</a></li>");
                 }
                 else
                 {
-                    aWriter.WriteLine(@"<li><a href=""{0}.html"">{0}</a></li>", year.TableName );
+                    htmlWriter.WriteLine($"<li><a href=\"{year.TableName}.html\">{year.TableName}</a></li>" );
                 }
             }
 
             var latestYear = DateTime.Now.Year.ToString();
-            aWriter.WriteLine(navbarEnd, latestYear);
+            htmlWriter.WriteLine(navbarEnd, latestYear);
         }
 
         /// <summary>
         /// Write details for a single sermon (single row in the spreadsheet/DataTable)
         /// </summary>
-        //void WriteSermon(TextWriter aWriter, string aYear, DataRow aSermonDetails)
-        //{
-        //    // If we fail to parse the date, we assume that the row is empty and write no details at all
-        //    DateTime sermonDate;
-        //    if (DateTime.TryParse(aSermonDetails.ItemArray[kDateColumn].ToString(), out sermonDate))
-        //    {
-        //        aWriter.WriteLine("<TR>");
-
-        //        aWriter.WriteLine(@"<TD style=""width:100px"">");
-        //        aWriter.WriteLine(sermonDate.ToString("d MMM yyyy"));
-        //        aWriter.WriteLine("</TD>");
-
-        //        aWriter.WriteLine("<TD>");
-        //        aWriter.WriteLine("<P class=\"SANSSERIF\">");
-        //        aWriter.WriteLine("<B>" + aSermonDetails.ItemArray[kTitleColumn].ToString() + "</B>");
-        //        string series = aSermonDetails.ItemArray[kSeriesColumn].ToString();
-        //        if (series.Length > 0)
-        //        {
-        //            aWriter.WriteLine(" (" + series + ")");
-        //        }
-        //        aWriter.WriteLine("</P>");
-        //        string scripture = aSermonDetails.ItemArray[kScriptureColumn].ToString();
-        //        if (scripture.Length > 0)
-        //        {
-        //            scripture += ", ";
-        //        }
-        //        aWriter.WriteLine(scripture + aSermonDetails.ItemArray[kSpeakerColumn].ToString());
-        //        aWriter.WriteLine("<BR>");
-        //        aWriter.WriteLine("<I>" + aSermonDetails.ItemArray[kDescriptionColumn].ToString() + "</I>");
-        //        aWriter.WriteLine("<P class=\"SANSSERIF\">");
-        //        DateTime duration;
-        //        string durationString = string.Empty;
-        //        if (DateTime.TryParse(aSermonDetails.ItemArray[kDurationColumn].ToString(), out duration))
-        //        {
-        //            durationString = " duration: " + duration.ToLongTimeString();
-        //        }
-        //        string image = @"<IMG src=""speaker.png"" alt=""play"" height=""16"" width=""16"">";
-        //        string audio = aSermonDetails.ItemArray[kFileColumn].ToString();
-        //        ValidateAudio(audio, aYear);
-        //        string networkDriveLocation = Properties.Settings.Default.NetworkDriveLocation; // Where the MP3 resources are located on the internet
-        //        string link = string.Format(@"<A href=""{0}/{1}/{2}"" target=""_blank"">{3} {2}</A>",
-        //                                     networkDriveLocation, aYear, audio, image);
-        //        //link = string.Format(@"<A href=""{1}/{2}"" target=""_blank"">{3} {2}</A>",
-        //        //                             networkDriveLocation, aYear, audio, image);
-        //        aWriter.WriteLine(link + durationString);
-        //        aWriter.WriteLine("</P>");
-        //        aWriter.WriteLine("</TD>");
-
-        //        aWriter.WriteLine("</TR>");
-        //    }
-        //}
-
-        /// <summary>
-        /// Write details for a single sermon (single row in the spreadsheet/DataTable)
-        /// </summary>
-        void WriteSermon(TextWriter aWriter, string aYear, DataRow aSermonDetails)
+        void WriteSermon(TextWriter htmlWriter, string year, DataRow sermonDetails)
         {
             // If we fail to parse the date, we assume that the row is empty and write no details at all
             DateTime sermonDate;
-            if (!DateTime.TryParse(aSermonDetails.ItemArray[kDateColumn].ToString(), out sermonDate))
+            if (!DateTime.TryParse(sermonDetails.ItemArray[kDateColumn].ToString(), out sermonDate))
             {
                 return;
             }
 
             // Skip the entry if the audio is not present, eg, no recording, or if we recorded but agreed not to publish the message
-            string audio = aSermonDetails.ItemArray[kFileColumn].ToString();
-            if (!ValidateAudio(audio, aYear))
+            string audio = sermonDetails.ItemArray[kFileColumn].ToString();
+            if (!ValidateAudio(audio, year))
             {
                 return;
             }
 
-            aWriter.WriteLine(@"<div class=""well"">");
+            htmlWriter.WriteLine("<div class=\"well\">");
 
-            aWriter.WriteLine(@"<div class=""row"">");
-            string series = aSermonDetails.ItemArray[kSeriesColumn].ToString();
+            htmlWriter.WriteLine("<div class=\"row\">");
+            string series = sermonDetails.ItemArray[kSeriesColumn].ToString();
             if (series.Length > 0)
             {
-                aWriter.WriteLine(@"<H3>{0} <SMALL>({1})</SMALL></H3>", aSermonDetails.ItemArray[kTitleColumn].ToString(), series);
+                htmlWriter.WriteLine($"<H3>{sermonDetails.ItemArray[kTitleColumn].ToString()} <SMALL>({series})</SMALL></H3>" );
             }
             else
             {
-                aWriter.WriteLine(@"<H3>{0}</H3>", aSermonDetails.ItemArray[kTitleColumn].ToString());
+                htmlWriter.WriteLine($"<H3>{sermonDetails.ItemArray[kTitleColumn].ToString()}</H3>");
             }
-            aWriter.WriteLine(@"</div>");
+            htmlWriter.WriteLine("</div>");
 
-            aWriter.WriteLine(@"  <div class=""row"">");
-            aWriter.WriteLine(@"    <div class=""col-sm-3"" style=""background-color:lavender;"">");
-            aWriter.WriteLine(@"       <p class=""text-primary"">{0}</p>", sermonDate.ToString("d MMM yyyy"));
-            string scripture = aSermonDetails.ItemArray[kScriptureColumn].ToString();
+            htmlWriter.WriteLine("  <div class=\"row\">");
+            htmlWriter.WriteLine("    <div class=\"col-sm-3\" style=\"background-color:lavender;\">");
+            htmlWriter.WriteLine($"       <p class=\"text-primary\">{sermonDate.ToString("d MMM yyyy")}</p>" );
+            string scripture = sermonDetails.ItemArray[kScriptureColumn].ToString();
             if (scripture.Length > 0)
             {
-                aWriter.WriteLine(@"       <p>{0}</p>", scripture);
+                htmlWriter.WriteLine($"       <p>{scripture}</p>");
             }
-            aWriter.WriteLine(@"       <p>{0}</p>", aSermonDetails.ItemArray[kSpeakerColumn].ToString());
-            aWriter.WriteLine(@"    </div>");
-            aWriter.WriteLine(@"    <div class=""col-sm-1"">");
-            aWriter.WriteLine(@"    </div>");
-            aWriter.WriteLine(@"    <div class=""col-sm-8"">");
-            aWriter.WriteLine(@"      <p class=""text-info"">");
-            aWriter.WriteLine(@"        {0}", aSermonDetails.ItemArray[kDescriptionColumn].ToString());
-            aWriter.WriteLine(@"      </p>");
-            aWriter.WriteLine(@"      <p>");
+            htmlWriter.WriteLine($"       <p>{sermonDetails.ItemArray[kSpeakerColumn].ToString()}</p>");
+            htmlWriter.WriteLine("    </div>");
+            htmlWriter.WriteLine("    <div class=\"col-sm-1\">");
+            htmlWriter.WriteLine("    </div>");
+            htmlWriter.WriteLine("    <div class=\"col-sm-8\">");
+            htmlWriter.WriteLine("      <p class=\"text-info\">");
+            htmlWriter.WriteLine($"        {sermonDetails.ItemArray[kDescriptionColumn].ToString()}");
+            htmlWriter.WriteLine("      </p>");
+            htmlWriter.WriteLine("      <p>");
 
-            string networkDriveLocation = Properties.Settings.Default.NetworkDriveLocation; // Where the MP3 resources are located on the internet
-                                                                                            //aWriter.WriteLine(@"    <a href=""{0}/{1}/{2}"" target=""_blank"" class=""btn btn-info"">", networkDriveLocation, aYear, audio);
-            aWriter.WriteLine(@"    <a href=""{1}/{2}"" target=""_blank"" class=""btn btn-info"">", networkDriveLocation, aYear, audio);
-            aWriter.WriteLine(@"      <span class=""glyphicon glyphicon-play""></span> Play");
-            aWriter.WriteLine(@"    </a>");
+            htmlWriter.WriteLine($"    <a href=\"{year}/{audio}\" target=\"_blank\" class=\"btn btn-info\">");
+            htmlWriter.WriteLine("      <span class=\"glyphicon glyphicon-play\"></span> Play");
+            htmlWriter.WriteLine("    </a>");
 
-            aWriter.WriteLine(@"      </p>");
+            htmlWriter.WriteLine("      </p>");
             DateTime duration;
             string durationString = string.Empty;
-            if (DateTime.TryParse(aSermonDetails.ItemArray[kDurationColumn].ToString(), out duration))
+            if (DateTime.TryParse(sermonDetails.ItemArray[kDurationColumn].ToString(), out duration))
             {
-                aWriter.WriteLine(@"      <p class=""text-info"">");
-                aWriter.WriteLine(@"        duration: " + duration.ToLongTimeString());
-                aWriter.WriteLine(@"      </p>");
+                htmlWriter.WriteLine("      <p class=\"text-info\">");
+                htmlWriter.WriteLine($"        duration: {duration.ToLongTimeString()}");
+                htmlWriter.WriteLine("      </p>");
             }
-            aWriter.WriteLine(@"    </div>");
-            aWriter.WriteLine(@"</div>");
+            htmlWriter.WriteLine("    </div>");
+            htmlWriter.WriteLine("</div>");
 
-            aWriter.WriteLine(@"</div>");
+            htmlWriter.WriteLine("</div>");
         }
 
-        bool ValidateAudio( string aAudio, string aYear )
+        bool ValidateAudio( string audio, string year )
         {
-            string fullPath = Path.Combine( Properties.Settings.Default.LocalDriveLocation, aYear, aAudio );
+            string fullPath = Path.Combine( Properties.Settings.Default.LocalDriveLocation, year, audio );
             var valid = File.Exists(fullPath);
             if ( !valid )
             {
-                iTextBoxStatus.Text += ( "File " + aAudio + " not present" + Environment.NewLine );
+                iTextBoxStatus.Text += ( $"File {audio} not present" + Environment.NewLine );
             }
 
             return valid;
